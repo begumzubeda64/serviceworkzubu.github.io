53ddc3d924e75dff65cead74e75537a99831a972
Zubeda Abbas <bzubeda64@gmail.com>
2020-08-10 00:55:45 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//make sure service workers are supported
+if('serviceWorker' in navigator){
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+        .register('../sw_cached_site.js')
+        .then(reg => console.log("Service worker registered"))
+        .catch(err => console.log(`Service Worker: Error: ${err}`))
+    });
+}
---
+++ sw_cached_pages.js
@@ -0,0 +1,52 @@
+const cacheName = 'v1';
+
+const cacheAssets = [
+    'css/bootstrap.css',
+    'js/bootstrap.js',
+    'js/popper.js',
+    'js/jquery.js',
+    'js/main.js',
+    'home.html',
+    'about.html'
+];
+
+//Call Install event
+self.addEventListener('install', e => {
+    console.log("Service worker: Installed");
+
+    e.waitUntil(
+        caches.open(cacheName)
+            .then(cache => {
+                console.log('Service Worker: Caching Files')
+                cache.addAll(cacheAssets)
+                .then(() => console.log('Assets added to cache'))
+                .catch(err => console.log('Error while fetching assets', err));
+            })
+            .then(() => self.skipWaiting())
+    );
+});
+
+//Call Activate Event
+self.addEventListener('activate', e => {
+    console.log("Service worker: Activated");
+    //remove unwanted caches
+    e.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if(cache !== cacheName){
+                        console.log('Service Worker: Clearing old cache');
+                        return caches.delete(cache);
+                    }
+                })
+            )
+        })
+    );
+});
+
+//Call Fetch Event
+self.addEventListener('fetch', e => {
+    console.log('Service worker: Fetching');
+    e.respondWith(
+        fetch(e.request).catch(() => caches.match(e.request)));
+});
